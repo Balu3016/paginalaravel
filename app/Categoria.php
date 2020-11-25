65474d6fcdb794a7c60fe6ad599da8debdb8ba73
Gustavo Garcia Figueroa <gustavogarcis30@gmail.com>
2020-11-25 14:43:41 -0600

    Categoria

---
+++ app/Categoria.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+   protected $table= 'Categoria';
+   protected $primarykey='idcategora';
+
+   public $timestamps='false';
+
+protected $fillable=  [
+'nombre',
+'descripcion',
+'condicion'
+
+
+
+];
+
+protected$guarded =[
+
+
+
+];
+
+}
